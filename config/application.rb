d564f2762a32b4b2d62207d12606cff5554ea8f8
Bruno Parga <brunoparga@gmail.com>
2019-12-04 14:55:01 -0300

    Configure API to allow CORS

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,13 @@ module FaveJobsApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins %r{\Ahttp://localhost:300\d\z} # Allow more than one port
+        resource '*', headers: :any, methods: %i[get post options]
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
