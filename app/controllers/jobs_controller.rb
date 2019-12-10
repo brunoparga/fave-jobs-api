b3debbf9d7b5b5e87af962bb0ea37fef98c53fc7
Bruno Parga <brunoparga@gmail.com>
2019-12-10 17:34:17 -0300

    Implement un-favoriting job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,4 +5,13 @@ class JobsController < ApplicationController
   def index
     render json: Job.all
   end
+
+  def create
+    p params
+    render json: Job.create(job_params)
+  end
+
+  def destroy
+    render json: Job.find_by(api_id: params[:api_id]).destroy
+  end
 end
